2477fc3c673925b6cdbec476a587dce3269c68fd
Zcracker <zcracker@163.com>
2023-04-13 14:03:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,162 @@
+openwrt官方源代码RTC时钟不工作问题会导致luci-app-statistics工作不正常，修改target/linux/mvebu/
+
+config-5.10
+config-5.15
+
+22.03分支添加或者修改以下行
+
+```
+# CONFIG_RTC_DRV_ARMADA38X is not set
+  CONFIG_RTC_INTF_DEV_UIE_EMUL=y
+  CONFIG_RTC_DRV_DS1307=y
+
+```
+
+这个方法在22.03分支中适用
+
+Master修改添加修改以下行， 
+
+```
+
+  CONFIG_RTC_DRV_DS1307=y
+  CONFIG_CLKDEV_LOOKUP=y 
+  CONFIG_RTC_INTF_DEV_UIE_EMUL=y
+# CONFIG_RTC_DRV_ARMADA38X is not set
+
+```
+
+make menuconfig选中moduleKernel modules > Other modules --> kmod-rtc-ds1307
+
+官方更新了903补丁和dts文件来控制风扇转速，风扇转速档位分别为80 102 170 230 255 链接如下
+
+https://github.com/openwrt/openwrt/commit/ea33a5def506b0ba647f779e60e6ccca03c29a17
+https://github.com/openwrt/openwrt/commit/aa41f4a395bda69cb6ef0ef423e2a4077284fbcd
+
+拷贝master分支以下文件到22.03目录下编译
+
+patches-5.10/903-drivers-hwmon-Add-the-IEI-WT61P803-PUZZLE-HWMON-driv.patch
+
+target/linux/mvebu/files/arch/arm64/boot/dts/marvell/cn9131-puzzle-m901.dts
+
+target/linux/mvebu/files/arch/arm64/boot/dts/marvell/cn9132-puzzle-m902.dts
+
+target/linux/mvebu/files/arch/arm64/boot/dts/marvell/puzzle-thermal.dtsi
+
+22.03和master风扇控制没有问题，但是iei-wt61p803-puzzle serial0-0: Command reply receive timeout，仍然弹出，LED报错，
+
+```
+[167591.374374] iei-wt61p803-puzzle serial0-0: Command reply receive timeout
+[167591.381197] leds green:cloud: Setting an LED's brightness failed (-110)
+```
+
+可以拷贝911的补丁到 
+target/linux/mvebu/patches-5.10/
+target/linux/mvebu/patches-5.15/
+目录下编译后，测试不会弹出iei-wt61p803-puzzle serial0-0: Command reply receive timeout
+
+可以使用cat /sys/class/hwmon/hwmon6/pwm1来查看档位
+
+```
+root@Puzzle-M901:~# cat /sys/class/hwmon/hwmon6/pwm1
+80
+```
+sensors命令来查看温度以及风扇转速
+
+```
+root@Puzzle-M901:~# sensors
+iei_wt61p803_puzzle-isa-f2702000
+Adapter: ISA adapter
+fan1:        1560 RPM
+fan2:           0 RPM
+fan3:           0 RPM
+fan4:           0 RPM
+fan5:           0 RPM
+temp1:        +40.0°C
+temp2:        +36.0°C
+```
+
+系统时间设置，编译时候选择hwclock程序编译   刷入后连接网络后同步浏览器时间后
+
+执行hwclock -w -v即可，查找rtc是否正常，执行hwclcok -w -v 后未出现以下错误，说明rtc工作正常
+
+
+```
+Waiting for clock tick...
+ioctl(3, RTC_UIE_ON, 0): Invalid argument
+
+```
+
+设置usb SSD MMC启动顺序如下 902修改cn9131-puzzle-m901为cn9132-puzzle-m902
+
+```
+setenv ieiusb 'ext4load usb 0:1 0x6500000 Image; ext4load sub 0:1 0x6000000 cn9131-puzzle-m901.dtb;setenv bootargs root=/dev/sda2 rw rootwait rootfstype=ext4 $console cpuidle.off=1; booti 0x6500000 - 0x6000000'
+
+setenv ieinvme 'ext4load nvme 0:1 0x6500000 Image; ext4load nvme 0:1 0x6000000 cn9131-puzzle-m901.dtb;setenv bootargs root=/dev/nvme0n1p2 rw rootwait rootfstype=ext4 $console cpuidle.off=1; booti 0x6500000 - 0x6000000'
+
+setenv ieimmc 'ext4load mmc 0:1 0x6500000 Image; ext4load mmc 0:1 0x6000000 cn9131-puzzle-m901.dtb;setenv bootargs root=/dev/mmcblk0p3 rw rootwait rootfstype=ext4 $console cpuidle.off=1; booti 0x6500000 - 0x6000000'
+
+setenv bootcmd 'usb reset;nvme scan;if ext4load usb 0:1 0x6000000 cn9131-puzzle-m901.dtb;then run ieiusb;fi;if ext4load nvme 0:1 0x6000000 cn9131-puzzle-m901.dtb;then run ieinvme;else run ieimmc;fi'
+
+saveenv
+
+```
+
+测试usb 启动代码  如下  rufus-3.22写入 openwrt-22.03-mvebu-cortexa72-iei_puzzle-m901-ext4-sdcard.img.gz文件 usb或者ssd
+ssd参照usb以下代码测试启动
+
+```
+usb reset
+ext4load usb 0:1 0x6000000 cn9131-puzzle-m901.dtb
+ext4load usb 0:1 0x6500000 Image 
+setenv bootargs root=/dev/sda2 rw rootwait rootfstype=ext4 $console cpuidle.off=1; booti 0x6500000 - 0x6000000
+
+```
+
+RTC硬件时钟修复后luci-app-statistics也自动恢复正常，，无数据跟RTC时间有关，项目中风扇控制脚本作废
+
+luci-app-statistics无数据发生的原因链接如下，可参考
+
+https://github.com/openwrt/packages/pull/10383
+
+https://forum.openwrt.org/t/collectd-rrdtool-data-corruption-warning-due-to-system-time-change/47223/11
+
+ 
+
+iEi-Puzzle-M901升级OpenWrt22.03后内核日志提示iei-wt61p803-puzzle serial0-0: Command reply receive timeout
+
+导致内核日志LED报错
+
+本目录中的补丁是客服给的，拷贝911补丁至target/linux/mvebu/目录下编译即可，iei开发组已经向openwrt官方提交补丁，，官方已经审核批准合并
+链接如下
+https://github.com/openwrt/openwrt/pull/12370
+https://git.openwrt.org/?p=openwrt/openwrt.git
+
+
+经过测试，升级固件尽量使用ext4格式固件，squashfs格式有时候会造成系统启动时候卡住无法启动，iEi-Puzzle-M901更新uboot方法
+
+```
+cp flash-image_m901-mmc.bin 到ext4 的 USB 里
+
+Marvell>> usb start
+
+Marvell>> ext4ls usb 0:1 确认文件有在USB 中
+
+<DIR>       4096 .
+<DIR>       4096 ..
+<DIR>      16384 lost+found
+
+1805808 flash-image_m901-mmc.bin
+
+以下为刷写uboot 到spi flash 里
+
+Marvell>> bubt flash-image_m901-mmc.bin spi usb
+
+重开机后会看到日期是新的
+
+U-Boot 2019.10-10.3.4.0-4 (Mar 27 2023 - 20:00:46 +0800)
+```
+
+
+
+
+
---
+++ fancontrol.sh
@@ -0,0 +1,207 @@
+#!/bin/sh
+
+# OpenWRT fan control
+
+# SLEEP_DURATION and CPU_TEMP_CHECK need to be multiples of each other
+EMERGENCY_COOLDOWN_DURATION=30
+SLEEP_DURATION=5
+CPU_TEMP_CHECK=20
+DEFAULT_SPEED=100
+EMERGENCY_COOLDOWN_TEMP_CHANGE=5                         
+
+# DON'T MESS WITH THESE
+VERBOSE=0
+LAST_FAN_SPEED=$DEFAULT_SPEED
+EMERGENCY_COOLDOWN=0                
+EMERGENCY_COOLDOWN_TIMER=0                         
+ELAPSED_TIME=0 
+CPU_TEMP=0
+
+# determine verbose mode
+if [ ! -z "$1" ]; then
+    VERBOSE=1
+fi
+
+echo "determin fan controller"
+# determine fan controller
+if [ -d /sys/class/hwmon/hwmon6 ]; then
+    FAN_CTRL=/sys/class/hwmon/hwmon6/pwm1
+elif [ -d /sys/devices/platform/pwm_fan ]; then
+    FAN_CTRL=/sys/devices/platform/pwm_fan/pwm1
+else
+    exit 0
+fi
+echo "start fan control"
+# retrieve new cpu temps
+get_temps() {
+    CPU_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon6/temp1_input`  
+}
+
+# use this to make setting the fan a bit easier
+#     set_fan WHAT VALUE
+set_fan() {
+    LAST_FAN_SPEED=`cat ${FAN_CTRL}`
+
+    if [ $LAST_FAN_SPEED -ne $2 ]; then
+        if [ $VERBOSE == 1 ]; then
+            echo "setting fan to ${2} (${1}) ${FAN_CTRL}"
+        fi
+
+        # write the new speed to the fan controller
+        echo $2 > ${FAN_CTRL}
+    else
+        if [ $VERBOSE == 1 ]; then
+            echo "keeping fan speed at ${LAST_FAN_SPEED}"
+        fi
+    fi
+}
+
+# floating-point greater-than-or-equals-to using awk 'cause ash doesn't
+# like floats. instead of this:
+#     if [ $VALUE_1 >= $VALUE_2 ];
+# use this:
+#     if [ $(fge $VALUE_1 $VALUE_2) == 1 ];
+float_ge() {
+    awk -v n1=$1 -v n2=$2 "BEGIN { if ( n1 >= n2 ) exit 1; exit 0; }"
+    echo $?
+}
+
+# start the emergency cooldown mode
+start_emergency_cooldown() {
+    if [ $VERBOSE == 1 ]; then
+        echo
+        echo "Starting Emergency Cooldown!"
+    fi
+
+    # toggle the cooldown bit to on and reset the timer
+    EMERGENCY_COOLDOWN=1
+    EMERGENCY_COOLDOWN_TIMER=$EMERGENCY_COOLDOWN_DURATION
+
+    set_fan EMERGENCY 255
+}              
+
+# check for load averages above 1.0
+check_load() {
+    # loop over each load value (1 min, 5 min, 15 min)
+    for LOAD in `cat /proc/loadavg | cut -d " " -f1,2,3`; do
+        if [ $VERBOSE == 1 ]; then
+            echo "Checking Load ${LOAD}"
+        fi
+
+        # trigger the emergency cooldown if we're using more than 1 core
+        if [ $(float_ge $LOAD 2.5) == 1 ]; then
+            start_emergency_cooldown
+
+            break
+        fi
+    done
+}
+
+# makes sure that the temperatures haven't fluctuated by more than 1.5 degrees
+check_temp_change() {
+    TEMP_CHANGE=$(($3 - $2));
+
+    if [ $VERBOSE == 1 ]; then
+        echo "${1} original temp: ${2} | new temp: ${3} | change: ${TEMP_CHANGE}"
+    fi
+
+    if [ $(float_ge $TEMP_CHANGE $EMERGENCY_COOLDOWN_TEMP_CHANGE) == 1 ]; then
+       start_emergency_cooldown;
+
+       continue;
+    fi
+}
+
+# set fan speeds based on CPU temperatures
+check_cpu_temp() {
+    if [ $VERBOSE == 1 ] ; then
+        echo "Checking CPU Temp ${CPU_TEMP}"
+    fi
+
+    if [ $CPU_TEMP -ge 70 ]; then
+        set_fan CPU 255
+    elif [ $(float_ge $CPU_TEMP 67.5) == 1 ]; then
+        set_fan CPU 223
+    elif [ $CPU_TEMP -ge 65 ]; then
+        set_fan CPU 191
+    elif [ $(float_ge $CPU_TEMP 62.5) == 1 ]; then
+        set_fan CPU 159
+    elif [ $CPU_TEMP -ge 60 ]; then
+        set_fan CPU 127
+    elif [ $CPU_TEMP -ge 55 ]; then
+        set_fan CPU 95
+    elif [ $CPU_TEMP -ge 50 ]; then
+        set_fan CPU 80
+    elif [ $CPU_TEMP -ge 45 ]; then
+        set_fan CPU 63
+    fi
+}
+
+# start the fan initially to $DEFAULT_SPEED
+set_fan START $DEFAULT_SPEED
+
+# and get the initial system temps
+get_temps
+
+# the main program loop:
+# - look at load averages every $SLEEP_DURATION seconds
+# - look at temperature deltas every $SLEEP_DURATION seconds
+# - look at raw cpu temp every $CPU_TEMP_CHECK seconds
+while true ; do
+
+    # handle emergency cooldown stuff
+    if [ $EMERGENCY_COOLDOWN == 1 ]; then
+
+        # reduce the number of seconds left in emergency cooldown mode
+        EMERGENCY_COOLDOWN_TIMER=$((${EMERGENCY_COOLDOWN_TIMER} - 5))
+
+        # do we still need to be in cooldown?
+        if [ $EMERGENCY_COOLDOWN_TIMER -le 0 ]; then
+
+            set_fan LAST $LAST_FAN_SPEED                              
+
+            EMERGENCY_COOLDOWN=0                                      
+
+            if [ $VERBOSE == 1 ]; then
+                echo "Exiting Emergency Cooldown Mode!"
+                echo
+            fi
+
+        else
+            if [ $VERBOSE == 1 ]; then
+                echo "Still in Emergency Cooldown. ${EMERGENCY_COOLDOWN_TIMER} seconds left."
+            fi
+
+            sleep $SLEEP_DURATION
+
+            continue
+        fi
+    fi
+
+    # save the previous temperatures                                    
+    LAST_CPU_TEMP=$CPU_TEMP                                                                                                  
+
+    # and re-read the current temperatures
+    get_temps 
+
+    # check the load averages
+    check_load
+
+    # check to see if the cpu, ram, or wifi temps have spiked
+    check_temp_change CPU $CPU_TEMP $LAST_CPU_TEMP
+
+    # check the raw CPU temps every $CPU_TEMP_CHECK seconds...
+    if [ $(( $ELAPSED_TIME % $CPU_TEMP_CHECK )) == 0 ]; then
+        check_cpu_temp
+    fi
+
+    # wait $SLEEP_DURATION seconds and do this again
+    if [ $VERBOSE == 1 ]; then
+        echo "waiting ${SLEEP_DURATION} seconds..."
+        echo
+    fi
+
+    sleep $SLEEP_DURATION;
+
+    ELAPSED_TIME=$(($ELAPSED_TIME + $SLEEP_DURATION))
+done
